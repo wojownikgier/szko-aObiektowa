e1eb5f34cdb7774ae67d0483b632f214469d4d3b
wojownikgier <kacperistelskixd@gmail.com>
2024-09-18 11:35:58 +0200

    dwa commit

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Osoba OsobaJas = new Osoba("Jaś" ,"fałdek","7");
-        //niezgodne z zasadna hermetyzacji
-        System.out.println(OsobaJas.imie);
-        System.out.println(OsobaJas.nazwisko);
-        System.out.println(OsobaJas.wiek);
-        //powino byt tak ze program nie mógł zmienic wałasnosci klas
+        System.out.println(OsobaJas.getImie());
+        System.out.println(OsobaJas.getNazwisko());
+        System.out.println(OsobaJas.getWiek());
     }
-
 }
--- src/Osoba.java
+++ src/Osoba.java
@@ -1,14 +1,37 @@
 public class Osoba {
-    String imie;
-    String nazwisko;
-    String wiek;
+private String imie;
+private String nazwisko;
+private String wiek;
 
     //konstruktor to metoda wywołania w momencie tworzenia osoby
 
     public Osoba(String imie, String nazwisko, String wiek) {
         this.imie = imie;
-
         this.nazwisko = nazwisko;
+        this.wiek = wiek;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(String wiek) {
         this.wiek = wiek;
     }
 }
